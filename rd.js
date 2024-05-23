b5b88e5a7cc72f26700281eae59eb289379ba4d0
moekak <98260675+moekak@users.noreply.github.com>
2024-05-23 10:16:51 +0900

    editing

--- rd.js
+++ rd.js
@@ -45,27 +45,25 @@ fetchTagData()
 
       // タグheadを実行
       executeScript(tagHead)
-      document.head.innerHTML += tagHead
+      // document.head.innerHTML += tagHead
       // タグbodyはDOMが完全にロードされてから実行さる
 
 
       // HTMLの解析が完了している場合、executeScript 関数を直ちに実行
       if (document.readyState !== 'loading') {
-        document.body.innerHTML += tagBody
-        executeScript(tagBody, document.body);
+        // document.body.innerHTML += tagBody
+        executeScript2(tagBody, document.body);
         // もしドキュメントの状態がまだ 'loading' の場合、DOMContentLoaded イベントが発火するのを待ち、その時点で executeScript 関数を実行
       } else {
         window.addEventListener('DOMContentLoaded', () => {
-          document.body.innerHTML += tagBody
-          executeScript(tagBody, document.body);
+          // document.body.innerHTML += tagBody
+          executeScript2(tagBody, document.body);
         });
       }
     }
     
   })
-  .catch((error) => {
 
-  });
 
   const executeScript = (data)=>{
 
@@ -83,17 +81,58 @@ fetchTagData()
     // <script> タグを抽出
     var scripts = doc.getElementsByTagName("script");
 
+    for (var i = 0; i < scripts.length; i++) {
   
+      if(scripts[i].async == true){
+        const newScript = document.createElement('script');
+        newScript.src = scripts[i].src;
+        newScript.async = true;
+        document.head.appendChild(newScript);
+      }else{
+          const newScript = document.createElement('script');
+          let scriptHTML = scripts[i].outerHTML.replace(/<script[^>]*>/i, "").replace(/<\/script>/i, "");
+          newScript.textContent = scriptHTML;
 
-    
+          document.head.appendChild(newScript)
+      }
+      
+    }
+  }
 
+  const executeScript2 = (data)=>{
+
+    //文字列をウェブページの一部として読むことができる特別なツール(DomParser)
+    var parser = new DOMParser();
+    // data変数に含まれるHTML文字列を解析し、DOM（Document Object Model）の構造に変換
+
+    /*
+
+    (例)    data = "<script>console.log("222222")</script>"
+
+              →<script><script>console.log("222222")</script> ドキュメントオブジェクトに変換したが、中身はセキュリティ上実行されない
+    */
+    var doc = parser.parseFromString(data, "text/html");
+    // <script> タグを抽出
+    var scripts = doc.getElementsByTagName("script");
+
+
+   
     // 抽出したスクリプトを実行
     for (var i = 0; i < scripts.length; i++) {
-      try {
-        eval(scripts[i].textContent);
-      } catch (error) {
+  
+      if(scripts[i].async == true){
+        const newScript = document.createElement('script');
+        newScript.src = scripts[i].src;
+        newScript.async = true;
+        document.body.appendChild(newScript);
+      }else{
+          const newScript = document.createElement('script');
+          let scriptHTML = scripts[i].outerHTML.replace(/<script[^>]*>/i, "").replace(/<\/script>/i, "");
+          newScript.textContent = scriptHTML;
 
+          document.body.appendChild(newScript)
       }
+      
     }
   }
 
